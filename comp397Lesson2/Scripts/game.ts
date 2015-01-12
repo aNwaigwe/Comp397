710b92e86ca15c7c316965bf039963334251711c
aNwaigwe <anpsisa@gmail.com>
2015-01-12 15:10:35 -0500

    Create a basic site structure

---
+++ comp397Lesson2/Scripts/game.ts
@@ -0,0 +1,42 @@
+﻿ 
+
+
+
+Class Player {
+    strenghth: number;
+    constructor() {
+        this.strenght = 10;
+    }
+
+    fights() {
+        console.log("He Fights");
+    }
+
+ }
+
+var stage: createjs.Stage;
+var canvas;
+var helloText;
+function init() {
+
+    canvas = document.getElementById("canvas");
+    stage = Createjs.Stage();
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameloop);
+
+     var tom = Player();
+    tom.fights();
+
+
+}
+
+function gameLoop() {
+    helloText.y += 1;
+    helloText.rotation =+=1;
+    stage.update();
+}
+
+funtion main() {
+    var helloText = new createjs.Text("Hello world", "40px", "#ffff00");
+    stage.addChild
+}
